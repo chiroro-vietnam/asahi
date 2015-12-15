9d5f331cd5e8619d94dad1a042f86e401349e226
chiroro-vietnam <phuong.nd@chiroro.com.vn>
2015-12-15 10:25:47 +0700

    update

--- app/Http/Controllers/Admin/SellingController.php
+++ app/Http/Controllers/Admin/SellingController.php
@@ -201,7 +201,7 @@ class SellingController extends Controller
                 $inputData['url']                       = Input::get('url');
                  $inputData['open_tab']                 = Input::get('open_tab');                
                 $inputData['display']                   = $display;
-                $inputData['display_top']		= $display_top;               
+                $inputData['display_top']		        = $display_top;               
                 $inputData['updated_at']                = date('Y-m-d H:i:s');
                 
                 $cs_id          = Input::get('cat_product_id');    
@@ -245,7 +245,6 @@ class SellingController extends Controller
                         $file->move(public_path().'/uploads/files/sell_product/', $fileName);                      
                         $inputData['file'] = '/uploads/files/sell_product/'.$fileName;
                 }
-
 
                 DB::table('sell_product')
                         ->where('id', $id)
