c860307e98282f6c212827510705f03a77837ed9
chiroro-vietnam <dao.at@chiroro.com.vn>
2015-12-19 16:13:06 +0700

    update asahidb.sql

--- asahidb.sql
+++ asahidb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2015 at 03:53 AM
+-- Generation Time: Dec 19, 2015 at 09:59 AM
 -- Server version: 10.1.8-MariaDB
 -- PHP Version: 5.5.30
 
@@ -77,8 +77,7 @@ INSERT INTO `category_rental` (`id`, `name`, `display`, `is_deleted`, `order`, `
 (4, '塩ビパイプ挿入機', 1, 0, 4, '2015-12-18 02:11:15', '2015-12-18 02:11:15'),
 (5, '漏水探知機', 1, 0, 5, '2015-12-18 02:11:26', '2015-12-18 02:15:24'),
 (6, '各種テスト用機器', 1, 0, 6, '2015-12-18 02:11:37', '2015-12-18 02:11:37'),
-(8, 'その他', 0, 0, 7, '2015-12-18 02:29:35', '2015-12-18 02:29:35'),
-(10, 'SASA', 1, 0, 8, '2015-12-18 02:46:19', '2015-12-18 02:46:59');
+(8, 'その他', 0, 0, 7, '2015-12-18 02:29:35', '2015-12-18 03:23:15');
 
 -- --------------------------------------------------------
 
@@ -159,6 +158,42 @@ CREATE TABLE `top_page_show` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `top_page_show`
+--
+
+INSERT INTO `top_page_show` (`id`, `rental_product_id`, `sell_product_id`, `is_deleted`, `created_at`, `updated_at`) VALUES
+(1, 1, NULL, 0, '2015-12-18 04:12:48', '2015-12-18 04:12:48'),
+(2, 2, NULL, 0, '2015-12-18 04:13:38', '2015-12-18 04:13:38'),
+(3, 3, NULL, 0, '2015-12-18 04:14:50', '2015-12-18 04:14:50'),
+(4, NULL, NULL, 0, '2015-12-19 06:19:13', '2015-12-19 06:19:13'),
+(5, NULL, 1, 0, '2015-12-19 06:20:11', '2015-12-19 06:55:48'),
+(6, NULL, 2, 0, '2015-12-19 06:20:42', '2015-12-19 06:20:42'),
+(7, NULL, 3, 0, '2015-12-19 06:21:51', '2015-12-19 06:21:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(50, 'admin', 'admin@chiroro.com.vn', '$2y$10$vcFnKXBKGIfxtRKt5pVkneOymfr25T20p0wt9Sg0VXiusLwGzsQDu', 'TGJo4VXaAttYZmFSGusoJF7WsTSfdKSrtin4bCJLXcEbVXuIESqzDVGHMZqy', '2015-12-19 12:10:29', '2015-12-19 06:56:23');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -193,6 +228,13 @@ ALTER TABLE `top_page_show`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -205,22 +247,27 @@ ALTER TABLE `category_product`
 -- AUTO_INCREMENT for table `category_rental`
 --
 ALTER TABLE `category_rental`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `rental_product`
 --
 ALTER TABLE `rental_product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `sell_product`
 --
 ALTER TABLE `sell_product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `top_page_show`
 --
 ALTER TABLE `top_page_show`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
