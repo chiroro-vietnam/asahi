aa0720e7d040dccf3a6366939fe66513f64ff172
chiroro-vietnam <dao.at@chiroro.com.vn>
2015-12-21 17:13:18 +0700

    bugs fixed layout

--- resources/views/frontend/inquiry/confirm.blade.php
+++ resources/views/frontend/inquiry/confirm.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div id="breadcrumbs">
   <ul class="pan clear">
-    <li><a href="index.html" title="TOP" class="now">TOP</a></li>
+    <li><a href="<?php echo route('frontend.homepage'); ?>" title="TOP" class="now">TOP</a></li>
     <li>お問い合わせ</li>
   </ul>
 </div>
